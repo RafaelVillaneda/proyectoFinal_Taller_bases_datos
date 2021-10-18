16a10ef752b1ab5c3394a42617030a7b3864cca2
Rafael Eulalio Villaneda de la Torre <rafaeua2401@outlook.com>
2021-10-18 16:25:50 -0500

    Se termino de hacer la interfaz del login y se empezo a trabajar con la ventana principal

--- App_Escritorio/Proyecto_Taller_Bases_Datos/src/javaapplication10/JavaApplication10.java
+++ App_Escritorio/Proyecto_Taller_Bases_Datos/src/javaapplication10/JavaApplication10.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package javaapplication10;
+import Vistas.Login;
 
 /**
  *
@@ -15,7 +16,7 @@ public class JavaApplication10 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        new Login().setVisible(true);
     }
     
 }
